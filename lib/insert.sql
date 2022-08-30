12eddecc272ed56e4b4550e7861c2727b11bd978
Salome <sallynanjala254@gmail.com>
2022-08-30 14:20:42 +0300

    commit tree b8095ba378c15695a7fc2af5600a64ffc930c94b

---
+++ lib/insert.sql
@@ -0,0 +1,20 @@
+INSERT INTO bears(name,age,sex,color,temperament,alive)VALUES('Mr.Chocolate',1,'F','white','cool',"TRUE" );
+INSERT INTO bears(name,age,sex,color,temperament,alive)VALUES('Rowdy',2,'M','white','wild',"FALSE" );
+INSERT INTO bears(name,age,sex,color,temperament,alive)VALUES('Tabitha',3,'F','white','slow',"TRUE" );
+INSERT INTO bears(name,age,sex,color,temperament,alive)VALUES('Sergeant Brown',1,'M','white','fast',"TRUE" );
+INSERT INTO bears(name,age,sex,color,temperament,alive)VALUES('Melissa',4,'M','white','angry',"FALSE" );
+INSERT INTO bears(name,age,sex,color,temperament,alive)VALUES('Grinch',5,'F','white','smiley',"TRUE" );
+INSERT INTO bears(name,age,sex,color,temperament,alive)VALUES('Wendy',6,'F','white','cool',"FALSE" );
+INSERT INTO bears(name,age,sex,color,temperament,alive)VALUES(NULL,7,'M','white','cool',"TRUE" );
+
+
+
+
+
+
+
+
+
+
+
+
